f975e7c049ea347467e56956613afdc59e585a65
Peter Fadlovich <Peter.Fadlovich@analog.com>
2023-11-03 10:44:26 -0400

    first commit

---
+++ build/startup_max32655.d
@@ -0,0 +1,2 @@
+C:/MaximSDK/Examples/MAX32655/BLE_mcs/build/startup_max32655.o: \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32655/Source/GCC/startup_max32655.S
---
+++ build/svc_mcs.d
@@ -0,0 +1,27 @@
+C:/MaximSDK/Examples/MAX32655/BLE_mcs/build/svc_mcs.o: services/svc_mcs.c \
+ services/svc_mcs.h C:/MaximSDK/Libraries/Cordio/wsf/include/wsf_types.h \
+ c:\maximsdk\tools\gnutools\10.3\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\stdint.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\machine\_default_types.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\sys\features.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\_newlib_version.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\sys\_intsup.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\sys\_stdint.h \
+ c:\maximsdk\tools\gnutools\10.3\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ C:/MaximSDK/Libraries/Cordio/ble-host/include/att_api.h \
+ C:/MaximSDK/Libraries/Cordio/wsf/include/wsf_timer.h \
+ C:/MaximSDK/Libraries/Cordio/wsf/include/wsf_os.h \
+ C:/MaximSDK/Libraries/Cordio/wsf/include/wsf_types.h \
+ C:/MaximSDK/Libraries/Cordio/wsf/include/wsf_queue.h \
+ C:/MaximSDK/Libraries/Cordio/ble-host/include/att_defs.h \
+ C:/MaximSDK/Libraries/Cordio/ble-host/include/att_uuid.h \
+ C:/MaximSDK/Libraries/Cordio/ble-host/include/dm_api.h \
+ C:/MaximSDK/Libraries/Cordio/ble-host/include/hci_api.h \
+ C:/MaximSDK/Libraries/Cordio/wsf/include/hci_defs.h \
+ C:/MaximSDK/Libraries/Cordio/wsf/include/wsf_os.h \
+ C:/MaximSDK/Libraries/Cordio/wsf/include/util/bda.h \
+ C:/MaximSDK/Libraries/Cordio/ble-host/sources/stack/cfg/cfg_stack.h \
+ C:/MaximSDK/Libraries/Cordio/ble-host/include/smp_defs.h \
+ C:/MaximSDK/Libraries/Cordio/ble-host/include/sec_api.h \
+ C:/MaximSDK/Libraries/Cordio/wsf/include/util/bstream.h \
+ C:/MaximSDK/Libraries/Cordio/ble-profiles/sources/services/svc_cfg.h
